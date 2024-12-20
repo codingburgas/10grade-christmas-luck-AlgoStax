3cd5e0b61578d8166f5c316e904d17eabcf8c02f
SSStefanov2223 <SSStefanov22@codingburgas.bg>
2024-12-20 21:40:23 +0200

    Add backgrond and character to the main window and add movement to the character

--- Algostax/Algostax/src/main.cpp
+++ Algostax/Algostax/src/main.cpp
@@ -1,44 +1,175 @@
-#include "pch.h"
+#include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1024;
+    const int screenHeight = 1024;
+    InitWindow(screenWidth, screenHeight, "Algostax");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    Texture2D backgroundTexture = LoadTexture("../assets/backgrounds/clinic-background.png");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    Texture2D backFrames[2];
+    backFrames[0] = LoadTexture("../assets/doctor/back-walk1.png");
+    backFrames[1] = LoadTexture("../assets/doctor/back-walk2.png");
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    Texture2D sideFrames[2];
+    sideFrames[1] = LoadTexture("../assets/doctor/side-walk1.png");
+    sideFrames[0] = LoadTexture("../assets/doctor/side-walk2.png");
+
+    Texture2D frontFrames[2];
+    frontFrames[0] = LoadTexture("../assets/doctor/front-walk1.png");
+    frontFrames[1] = LoadTexture("../assets/doctor/front-walk2.png");
+
+    Texture2D doctorFront = LoadTexture("../assets/doctor/doctor-front.png");
+
+    int totalFrames = 2;
+    int currentFrame = 0;
+    float frameTimer = 0.0f;
+    const float frameSpeed = 0.4f;
+
+    Vector2 doctorPos = { -485, -900 };
+    float speed = 100.0f;
+    bool isMoving = false;
+    Vector2 velocity = { 0.0f, 0.0f };
+
+    float initiMovementTimer = 0.0f;
+    const float initMovementDuration = 1.0f;
+
+    float scale = 0.2f;
+
+    int targetWidth = (int)(backFrames[0].width * scale);
+    int targetHeight = (int)(backFrames[0].height * scale);
+
+    int standingWidth = (int)(doctorFront.width * scale);
+    int standingHeight = (int)(doctorFront.height * scale);
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        float deltaTime = GetFrameTime();
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        if (initiMovementTimer < initMovementDuration)
+        {
+            velocity = Vector2{ 0.0f, speed };
+
+            frameTimer += deltaTime;
+            if (frameTimer >= frameSpeed)
+            {
+                frameTimer = 0.0f;
+                currentFrame++;
+                if (currentFrame >= totalFrames) currentFrame = 0;
+            }
+
+            initiMovementTimer += deltaTime;
+        }
+        else
+        {
+            if (IsKeyDown(KEY_S))
+            {
+                velocity = Vector2{ 0.0f, -speed };
+                isMoving = true;
+            }
+            else if (IsKeyDown(KEY_W))
+            {
+                velocity = Vector2{ 0.0f, speed };
+                isMoving = true;
+            }
+            else if (IsKeyDown(KEY_D))
+            {
+                velocity = Vector2{ -speed, 0.0f };
+                isMoving = true;
+            }
+            else if (IsKeyDown(KEY_A))
+            {
+                velocity = Vector2{ speed, 0.0f };
+                isMoving = true;
+            }
+            else
+            {
+                velocity = Vector2{ 0.0f, 0.0f };
+                isMoving = false;
+            }
+        }
+
+        doctorPos.x += velocity.x * deltaTime;
+        doctorPos.y += velocity.y * deltaTime;
+
+        if (isMoving)
+        {
+            frameTimer += deltaTime;
+            if (frameTimer >= frameSpeed)
+            {
+                frameTimer = 0.0f;
+                currentFrame++;
+                if (currentFrame >= totalFrames) currentFrame = 0;
+            }
+        }
+
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawTexture(backgroundTexture, 0, 0, WHITE);
+
+        if (initiMovementTimer < initMovementDuration)
+        {
+            DrawTexturePro(backFrames[currentFrame],
+                Rectangle{ 0, 0, (float)backFrames[0].width, (float)backFrames[0].height },
+                Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
+                doctorPos, 0.0f, WHITE);
+        }
+        else if (isMoving)
+        {
+            if (velocity.y > 0)
+            {
+                DrawTexturePro(backFrames[currentFrame],
+                    Rectangle{ 0, 0, (float)backFrames[0].width, (float)backFrames[0].height },
+                    Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
+                    doctorPos, 0.0f, WHITE);
+            }
+            else if (velocity.y < 0)
+            {
+                DrawTexturePro(frontFrames[currentFrame],
+                    Rectangle{ 0, 0, (float)frontFrames[0].width, (float)frontFrames[0].height },
+                    Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
+                    doctorPos, 0.0f, WHITE);
+            }
+            else if (velocity.x < 0)
+            {
+                DrawTexturePro(sideFrames[0],
+                    Rectangle{ 0, 0, (float)sideFrames[0].width, (float)sideFrames[0].height },
+                    Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
+                    doctorPos, 0.0f, WHITE);
+            }
+            else if (velocity.x > 0)
+            {
+                DrawTexturePro(sideFrames[1],
+                    Rectangle{ 0, 0, (float)sideFrames[1].width, (float)sideFrames[1].height },
+                    Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
+                    doctorPos, 0.0f, WHITE);
+            }
+        }
+        else
+        {
+            DrawTexturePro(doctorFront,
+                Rectangle{ 0, 0, (float)doctorFront.width, (float)doctorFront.height },
+                Rectangle{ 0, 0, (float)standingWidth, (float)standingHeight },
+                doctorPos, 0.0f, WHITE);
+        }
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    UnloadTexture(backgroundTexture);
+    UnloadTexture(doctorFront);
+    for (int i = 0; i < 2; i++)
+    {
+        UnloadTexture(backFrames[i]);
+        UnloadTexture(sideFrames[i]);
+        UnloadTexture(frontFrames[i]);
+    }
+
+    CloseWindow();
 
     return 0;
 }
