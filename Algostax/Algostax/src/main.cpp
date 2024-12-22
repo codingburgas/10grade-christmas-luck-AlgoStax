aab901900fdf8059c1a95a187c9a16d8da78c040
SSStefanov2223 <SSStefanov22@codingburgas.bg>
2024-12-22 21:37:04 +0200

    Change raylib folder

--- Algostax/Algostax/src/main.cpp
+++ Algostax/Algostax/src/main.cpp
@@ -1,4 +1,4 @@
-#include "raylib.h"
+#include "pch.h"
 
 int main(void)
 {
@@ -12,44 +12,49 @@ int main(void)
     backFrames[0] = LoadTexture("../assets/doctor/back-walk1.png");
     backFrames[1] = LoadTexture("../assets/doctor/back-walk2.png");
 
-    Texture2D sideFrames[2];
-    sideFrames[1] = LoadTexture("../assets/doctor/side-walk1.png");
-    sideFrames[0] = LoadTexture("../assets/doctor/side-walk2.png");
+    Texture2D leftFrames[2];
+    leftFrames[0] = LoadTexture("../assets/doctor/side-walk2.png");
+    leftFrames[1] = LoadTexture("../assets/doctor/left-walk1.png");
+
+    Texture2D rightFrames[2];
+    rightFrames[0] = LoadTexture("../assets/doctor/side-walk1.png");
+    rightFrames[1] = LoadTexture("../assets/doctor/right-walk1.png");
 
     Texture2D frontFrames[2];
     frontFrames[0] = LoadTexture("../assets/doctor/front-walk1.png");
     frontFrames[1] = LoadTexture("../assets/doctor/front-walk2.png");
 
-    Texture2D doctorFront = LoadTexture("../assets/doctor/doctor-front.png");
+    Texture2D standingTexture = LoadTexture("../assets/doctor/doctor-front.png");
 
     int totalFrames = 2;
     int currentFrame = 0;
     float frameTimer = 0.0f;
     const float frameSpeed = 0.4f;
 
-    Vector2 doctorPos = { -485, -900 };
+    Vector2 characterPosition = { -485, -900 };
     float speed = 100.0f;
     bool isMoving = false;
     Vector2 velocity = { 0.0f, 0.0f };
 
-    float initiMovementTimer = 0.0f;
-    const float initMovementDuration = 1.0f;
+    float initialMovementTimer = 0.0f;
+    const float initialMovementDuration = 1.0f;
 
     float scale = 0.2f;
 
     int targetWidth = (int)(backFrames[0].width * scale);
     int targetHeight = (int)(backFrames[0].height * scale);
 
-    int standingWidth = (int)(doctorFront.width * scale);
-    int standingHeight = (int)(doctorFront.height * scale);
+    int standingWidth = (int)(standingTexture.width * scale);
+    int standingHeight = (int)(standingTexture.height * scale);
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+
         float deltaTime = GetFrameTime();
 
-        if (initiMovementTimer < initMovementDuration)
+        if (initialMovementTimer < initialMovementDuration)
         {
             velocity = Vector2{ 0.0f, speed };
 
@@ -61,7 +66,7 @@ int main(void)
                 if (currentFrame >= totalFrames) currentFrame = 0;
             }
 
-            initiMovementTimer += deltaTime;
+            initialMovementTimer += deltaTime;
         }
         else
         {
@@ -92,8 +97,8 @@ int main(void)
             }
         }
 
-        doctorPos.x += velocity.x * deltaTime;
-        doctorPos.y += velocity.y * deltaTime;
+        characterPosition.x += velocity.x * deltaTime;
+        characterPosition.y += velocity.y * deltaTime;
 
         if (isMoving)
         {
@@ -111,12 +116,12 @@ int main(void)
 
         DrawTexture(backgroundTexture, 0, 0, WHITE);
 
-        if (initiMovementTimer < initMovementDuration)
+        if (initialMovementTimer < initialMovementDuration)
         {
             DrawTexturePro(backFrames[currentFrame],
                 Rectangle{ 0, 0, (float)backFrames[0].width, (float)backFrames[0].height },
                 Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
-                doctorPos, 0.0f, WHITE);
+                characterPosition, 0.0f, WHITE);
         }
         else if (isMoving)
         {
@@ -125,47 +130,50 @@ int main(void)
                 DrawTexturePro(backFrames[currentFrame],
                     Rectangle{ 0, 0, (float)backFrames[0].width, (float)backFrames[0].height },
                     Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
-                    doctorPos, 0.0f, WHITE);
+                    characterPosition, 0.0f, WHITE);
             }
             else if (velocity.y < 0)
             {
                 DrawTexturePro(frontFrames[currentFrame],
                     Rectangle{ 0, 0, (float)frontFrames[0].width, (float)frontFrames[0].height },
                     Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
-                    doctorPos, 0.0f, WHITE);
+                    characterPosition, 0.0f, WHITE);
             }
             else if (velocity.x < 0)
             {
-                DrawTexturePro(sideFrames[0],
-                    Rectangle{ 0, 0, (float)sideFrames[0].width, (float)sideFrames[0].height },
+                DrawTexturePro(leftFrames[currentFrame],
+                    Rectangle{ 0, 0, (float)leftFrames[0].width, (float)leftFrames[0].height },
                     Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
-                    doctorPos, 0.0f, WHITE);
+                    characterPosition, 0.0f, WHITE);
             }
             else if (velocity.x > 0)
             {
-                DrawTexturePro(sideFrames[1],
-                    Rectangle{ 0, 0, (float)sideFrames[1].width, (float)sideFrames[1].height },
+                DrawTexturePro(rightFrames[currentFrame],
+                    Rectangle{ 0, 0, (float)rightFrames[0].width, (float)rightFrames[0].height },
                     Rectangle{ 0, 0, (float)targetWidth, (float)targetHeight },
-                    doctorPos, 0.0f, WHITE);
+                    characterPosition, 0.0f, WHITE);
             }
         }
         else
         {
-            DrawTexturePro(doctorFront,
-                Rectangle{ 0, 0, (float)doctorFront.width, (float)doctorFront.height },
+            DrawTexturePro(standingTexture,
+                Rectangle{ 0, 0, (float)standingTexture.width, (float)standingTexture.height },
                 Rectangle{ 0, 0, (float)standingWidth, (float)standingHeight },
-                doctorPos, 0.0f, WHITE);
+                characterPosition, 0.0f, WHITE);
         }
+
+        DrawRectangle(465, 355, 120, 300, RAYWHITE);
 
         EndDrawing();
     }
 
     UnloadTexture(backgroundTexture);
-    UnloadTexture(doctorFront);
+    UnloadTexture(standingTexture);
     for (int i = 0; i < 2; i++)
     {
         UnloadTexture(backFrames[i]);
-        UnloadTexture(sideFrames[i]);
+        UnloadTexture(leftFrames[i]);
+        UnloadTexture(rightFrames[i]);
         UnloadTexture(frontFrames[i]);
     }
 
